f2d964ca4163b0de585b20698ce5451ebae7b4fd
Realgrapejuice <66715905+realgrapejuice@users.noreply.github.com>
2021-04-29 18:13:58 +0900

    헤더 섹션의 레이아웃을 잡고 CSS를 입힘

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,28 +6,44 @@ const Nav = (props) => {
     <nav className={styles.nav}>
       <ul>
         <li className="nav__item">
-          <a href="/홈">홈</a>
+          <a className={styles.link} href="/홈">
+            홈
+          </a>
         </li>
         <li className="nav__item">
-          <a href="/시장">시장</a>
+          <a className={styles.link} href="/시장">
+            시장
+          </a>
         </li>
         <li className="nav__item">
-          <a href="/쇼핑">쇼핑</a>
+          <a className={styles.link} href="/쇼핑">
+            쇼핑
+          </a>
         </li>
         <li className="nav__item">
-          <a href="/콘텐츠">콘텐츠</a>
+          <a className={styles.link} href="/콘텐츠">
+            콘텐츠
+          </a>
         </li>
         <li className="nav__item">
-          <a href="/시세">시세</a>
+          <a className={styles.link} href="/시세">
+            시세
+          </a>
         </li>
         <li className="nav__item">
-          <a href="/도매">도매</a>
+          <a className={styles.link} href="/도매">
+            도매
+          </a>
         </li>
         <li className="nav__item">
-          <a href="/후기">후기</a>
+          <a className={styles.link} href="/후기">
+            후기
+          </a>
         </li>
         <li className="nav__item">
-          <a href="/문의">문의</a>
+          <a className={styles.link} href="/문의">
+            문의
+          </a>
         </li>
       </ul>
     </nav>
